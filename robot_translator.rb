771b3f7cccad69297f16d47c6b82b723f5248c2b
Abraham Kwak <kwak.abe@gmail.com>
2017-05-14 13:44:54 -0400

    created translate character method.

--- robot_translator.rb
+++ robot_translator.rb
@@ -6,20 +6,37 @@
 
 # If it's not a letter at all, it becomes "boing".
 
-# Otherwise, it becomes "bleep".
+# Otherwise, it becomes "beep".
 
-# "Happy Halloween!" => "bloopbeepbeepbeepbeepboingbloopbleepbleepbleepbleepbleepbuzzbuzzbeepboing"
+# "Happy Halloween!" => "bloopbeepbeepbeepbeepboingbloopbeepbeepbeepbeepbeepbuzzbuzzbeepboing"
 
-def translate_phrase(phrase)
-  char_index = 0
-  while char_index < phrase.length
-    puts phrase[char_index]
-    char_index += 1
+def translate_char(char)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  halfway = alphabet.length / 2
+  is_capitalized = (char.upcase == char)
+
+  if !alphabet.index(char.downcase)
+    "boing"
+  elsif is_capitalized && alphabet.index(char.downcase) < halfway
+    "bloop"
+  elsif is_capitalized || char == "e"
+    "buzz"
+  else
+    "beep"
   end
 end
 
-translate_phrase("Happy Halloween!")
+def translate_phrase(phrase)
+  char_index = 0
+  translated_response = ""
+  while char_index < phrase.length
+    translated_response << translate_char(phrase[char_index])
+    char_index += 1
+  end
+  translated_response
+end
 
+# User Interface
 
 
 
