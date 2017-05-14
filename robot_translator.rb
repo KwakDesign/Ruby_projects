49f083a862c65408e0052659771cf307a1ee444b
Abraham Kwak <kwak.abe@gmail.com>
2017-05-14 12:45:28 -0400

    created translate phrase method.

--- robot_translator.rb
+++ robot_translator.rb
@@ -10,3 +10,19 @@
 
 # "Happy Halloween!" => "bloopbeepbeepbeepbeepboingbloopbleepbleepbleepbleepbleepbuzzbuzzbeepboing"
 
+def translate_phrase(phrase)
+  char_index = 0
+  while char_index < phrase.length
+    puts phrase[char_index]
+    char_index += 1
+  end
+end
+
+translate_phrase("Happy Halloween!")
+
+
+
+
+
+
+
