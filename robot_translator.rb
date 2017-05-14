de5a72731eeba85db65bd6703adaf4adad674218
Abraham Kwak <kwak.abe@gmail.com>
2017-05-14 12:12:13 -0400

    intialize pseudo code for robot translator file.

---
+++ robot_translator.rb
@@ -0,0 +1,12 @@
+# Robot Translator Pseudo Code
+
+# If a letter is capitalized and it's in the first half of the alphabet, it becomes "bloop".
+
+# Otherwise, if a letter is capitalized or it's the letter "e", it becomes "buzz".
+
+# If it's not a letter at all, it becomes "boing".
+
+# Otherwise, it becomes "bleep".
+
+# "Happy Halloween!" => "bloopbeepbeepbeepbeepboingbloopbleepbleepbleepbleepbleepbuzzbuzzbeepboing"
+
